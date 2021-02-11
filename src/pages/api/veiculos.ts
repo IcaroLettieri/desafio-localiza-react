641ee8f0ef0254bd0f595c12c0dad77ab8106c9f
Douglas Moura <douglas.ademoura@gmail.com>
2021-02-10 22:26:21 -0300

    chore: adiciona interface ao gerador de veículos

--- src/pages/api/veiculos.ts
+++ src/pages/api/veiculos.ts
@@ -1,10 +1,25 @@
 import { NowResponse } from '@vercel/node';
 import faker from 'faker';
 
+type Combustivel = 'Álcool' | 'Gasolina' | 'Diesel';
+type Categoria = 'Básico' | 'Completo' | 'Luxo';
+
+interface IVeiculo {
+  id: number;
+  modelo: string;
+  marca: string;
+  placa: string;
+  valorHora: number;
+  capacidadePortaMalas: number;
+  capacidadeTanque: number;
+  combustivel: Combustivel;
+  categoria: Categoria;
+}
+
 const handler = (_, response: NowResponse) => {
-  const veiculos = [];
-  const combustiveis = ['Álcool', 'Gasolina', 'Diesel'];
-  const categorias = ['Básico', 'Completo', 'Luxo'];
+  const veiculos: IVeiculo[] = [];
+  const combustiveis: Combustivel[] = ['Álcool', 'Gasolina', 'Diesel'];
+  const categorias: Categoria[] = ['Básico', 'Completo', 'Luxo'];
 
   for (let i = 1; i < 101; i++) {
     veiculos.push({
@@ -12,7 +27,7 @@ const handler = (_, response: NowResponse) => {
       modelo: faker.vehicle.model(),
       marca: faker.vehicle.manufacturer(),
       placa: faker.vehicle.vin(),
-      valorHora: faker.commerce.price(15, 150),
+      valorHora: +faker.commerce.price(15, 150),
       capacidadePortaMalas: faker.random.number({ min: 250, max: 700 }),
       capacidadeTanque: faker.random.number({ min: 250, max: 700 }),
       combustivel: combustiveis[faker.random.number({ min: 0, max: 2 })],
