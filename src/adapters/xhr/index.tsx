e6f86cf5e1754cab9a162fd015d7baabbd435972
Douglas Moura <douglas.ademoura@gmail.com>
2021-02-12 19:19:53 -0300

    chore: cria adaptadores para interagir com a API

---
+++ src/adapters/xhr/index.tsx
@@ -0,0 +1,70 @@
+import axios from 'axios';
+import IVeiculo from '../../types/IVeiculo';
+
+export const api = axios.create({ baseURL: '/api', timeout: 2000 });
+
+export const getVeiculos = () => api.get<IVeiculo[]>('/veiculos');
+
+export const postAgendar = ({
+  dataAgendamento,
+  dataHoraColetaPrevista,
+  dataHoraColetaRealizada,
+  dataHoraEntregaPrevista,
+  dataHoraEntregaRealizada,
+  valorHora,
+  horasLocacao,
+  subTotal,
+  custosAdicionais,
+  valorTotal,
+  realizadaVistoria,
+}) => api.post('/agendar', {
+  dataAgendamento,
+  dataHoraColetaPrevista,
+  dataHoraColetaRealizada,
+  dataHoraEntregaPrevista,
+  dataHoraEntregaRealizada,
+  valorHora,
+  horasLocacao,
+  subTotal,
+  custosAdicionais,
+  valorTotal,
+  realizadaVistoria,
+});
+
+export const postDevolver = ({
+  id,
+  carroLimpo,
+  tanqueCheio,
+  tanqueLitroPendente,
+  amassados,
+  arranhoes,
+}) => api.post('/devolver', {
+  id,
+  carroLimpo,
+  tanqueCheio,
+  tanqueLitroPendente,
+  amassados,
+  arranhoes,
+});
+
+export const postLogin = ({ login, senha }) => api.post('/login', { login, senha });
+
+export const postCadastrar = ({
+  cpfMatricula,
+  nome,
+  tipoCadastro,
+  cep,
+  logradouro,
+  numero,
+  cidade,
+  estado,
+}) => api.post('/cadastrar', {
+  cpfMatricula,
+  nome,
+  tipoCadastro,
+  cep,
+  logradouro,
+  numero,
+  cidade,
+  estado,
+});
