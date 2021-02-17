479af9b11acbbb4eac538060522dbb522e090b65
icarolettieri <icaro_lettieri@live.com>
2021-02-17 20:21:39 -0300

    chore: inclusão do util de formato de data

---
+++ src/utils/formatDate.ts
@@ -0,0 +1,28 @@
+import { format } from 'date-fns';
+import pt from 'date-fns/locale/pt-BR';
+
+/* Example of use
+ * const date = new Date();
+ * formatDate(date, "dd'/'MM'/'yyyy'") -> return 01/12/2021
+ * formatDate(date, "yyyy'-'MM'-'dd'") -> return 2021-12-01
+ *
+ * formatDate(date, "dd'/'MM'/'yyyy'", 10) -> return 11/12/2021
+ * formatDate(date, "yyyy'-'MM'-'dd'", 10) -> return 2021-12-11
+ */
+
+const formatDate = (date: Date, toFormat: string, daysToIncrement?: number) => {
+  const dateParse = new Date(date);
+
+  if (daysToIncrement) {
+    dateParse.setDate(dateParse.getDate() + daysToIncrement);
+  }
+
+  const formattedDate = format(
+    dateParse,
+    toFormat,
+    { locale: pt },
+  );
+  return formattedDate;
+};
+
+export default formatDate;
