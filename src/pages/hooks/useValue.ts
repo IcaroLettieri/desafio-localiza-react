3e506021dd719b449c4a29cfc9a88083dcf2daf8
icarolettieri <icaro_lettieri@live.com>
2021-02-17 23:55:44 -0300

    chore: hooks de handle setstate

---
+++ src/pages/hooks/useValue.ts
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+import { ChangeEvent, useState } from 'react';
+
+export type IUseValue = [string, (event: ChangeEvent<HTMLInputElement>) => void];
+
+const useValue = (initialValue = ''): IUseValue => {
+  const [value, setValue] = useState(initialValue);
+
+  const handleOnChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setValue(event.currentTarget.value);
+  };
+
+  return [value, handleOnChange];
+};
+
+export default useValue;
