a5cdf5282487e6cb8f6dd9fa180d9164c7355cd4
icarolettieri <icaro_lettieri@live.com>
2021-02-19 22:57:55 -0300

    chore: criação de tipagem do cadastro

---
+++ src/types/ICadastro.ts
@@ -0,0 +1,14 @@
+interface ICadastro {
+  Cpf: string;
+  Nome: string;
+  Senha: string;
+  DataNascimento: Date;
+  Cep: string;
+  Logradouro: string;
+  Numero: number;
+  Complemento: string;
+  Cidade: string;
+  Estado: string;
+}
+
+export default ICadastro;
