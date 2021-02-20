3d47264a9a277092d14ef15be3ef6efb0876f9ad
Erick Santos <erickhenrique321@gmail.com>
2021-02-20 04:26:57 -0300

    chore: header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,10 @@
-import Search20 from '@carbon/icons-react/lib/search/20';
+import Report20 from '@carbon/icons-react/lib/report/20';
 import User20 from '@carbon/icons-react/lib/user--avatar/20';
-import Menu20 from '@carbon/icons-react/lib/menu/20';
 import {
   Header as CarbonHeader,
   HeaderName,
   HeaderGlobalAction,
   HeaderGlobalBar,
-  HeaderNavigation,
 } from 'carbon-components-react/lib/components/UIShell';
 import styles from './styles/Header.module.css';
 
@@ -25,17 +23,14 @@ const Header = () => {
 
   return (
     <CarbonHeader className={styles.header} aria-label={process.env.SITE_NAME}>
-      <HeaderName href="#" prefix="🚗">
-        Alugue seu carro
+
+      <HeaderName href="#" prefix="">
+        <img src="logonegativa.png" height={23} width={120} alt="logo localiza" />
       </HeaderName>
-      <HeaderNavigation aria-label="Menu principal">
-        <HeaderGlobalAction aria-label="Abrir menu" onClick={handleMenu}>
-          <Menu20 />
-        </HeaderGlobalAction>
-      </HeaderNavigation>
+
       <HeaderGlobalBar>
-        <HeaderGlobalAction aria-label="Pesquisar" onClick={handlePesquisar}>
-          <Search20 />
+        <HeaderGlobalAction aria-label="Agendamentos" onClick={handlePesquisar}>
+          <Report20 />
         </HeaderGlobalAction>
         <HeaderGlobalAction aria-label="Área do usuário" onClick={handleUsuario}>
           <User20 />
