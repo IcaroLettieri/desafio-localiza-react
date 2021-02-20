8bd795880b5f6253f1e926c34666ab745c752a9a
icarolettieri <icaro_lettieri@live.com>
2021-02-19 22:57:25 -0300

    chore: incremento no services com os retornos da api

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,9 +1,20 @@
 export const TOKEN_KEY = '@localiza-Token';
+export const USER_ID = '@localiza-UserID';
+export const USER_NAME = '@localiza-UserName';
+
 export const isAuthenticated = () => localStorage.getItem(TOKEN_KEY) !== null;
 export const getToken = () => localStorage.getItem(TOKEN_KEY);
-export const login = (token) => {
-  localStorage.setItem(TOKEN_KEY, token);
+export const getUserId = () => localStorage.getItem(USER_ID);
+export const getUserName = () => localStorage.getItem(USER_NAME);
+
+export const login = (entity) => {
+  localStorage.setItem(TOKEN_KEY, entity.token);
+  localStorage.setItem(USER_ID, entity.id);
+  localStorage.setItem(USER_NAME, entity.nome);
 };
+
 export const logout = () => {
   localStorage.removeItem(TOKEN_KEY);
+  localStorage.removeItem(USER_ID);
+  localStorage.removeItem(USER_NAME);
 };
